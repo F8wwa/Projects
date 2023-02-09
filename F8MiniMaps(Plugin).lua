17849607bf936ab070c42adbf7d0bc06a7192fd1
F8wwa <90915090+F8wwa@users.noreply.github.com>
2023-02-09 16:01:58 -0300

    Update F8MiniMaps(Plugin).lua

--- F8MiniMaps(Plugin).lua
+++ F8MiniMaps(Plugin).lua
@@ -1,1 +1,12 @@
+-- Neste arquivo você encotrara recursos adicionais para o plugin (F8MiniMaps)
+-- Template + Atualizada
 
+local GitHubResourcesValue = script.Parent.Parent.Parent["F8MiniMaps(Plugin)"].GitHubResources
+
+local funciton RecentTemplate()
+
+end
+
+if GitHubResourcesValue == "Template" then
+  
+end
